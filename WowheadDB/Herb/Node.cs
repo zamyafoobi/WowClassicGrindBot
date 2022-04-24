04a54c11707128bcadf663bc067fb07590cd59c2
Xian55 <367101+Xian55@users.noreply.github.com>
2022-04-24 15:52:00 +0200

    WoWheadDB: Include gatherable nodes such as herbs and veins for the given Area(Zone).

---
+++ WowheadDB/Herb/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using SharedLib.Extensions;
+
+namespace WowheadDB
+{
+    public class Node
+    {
+        public List<List<float>> coords;
+        public int level;
+        public string name;
+        public int type;
+        public int id;
+
+        public List<Vector3> points => VectorExt.FromList(coords);
+    }
+}
