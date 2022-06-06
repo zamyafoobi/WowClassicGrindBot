b4fbeda6027976528754a8e2e6cbd1fcafbb135d
Xian55 <367101+Xian55@users.noreply.github.com>
2022-06-06 22:35:50 +0200

    PathingAPI: Start app with localhost and for now to make it work verbose/trace logging enabled!

--- PathingAPI/Program.cs
+++ PathingAPI/Program.cs
@@ -1,19 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace PathingAPI
 {
     public class Program
     {
-        public static string hostUrl = "http://0.0.0.0:5001";
+        public static string hostUrl = "http://127.0.0.1:5001";
 
         public static void Main(string[] args)
         {
