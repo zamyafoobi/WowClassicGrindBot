7e1d14896bc674791011055f5341e74a864d9758
Xian55 <367101+Xian55@users.noreply.github.com>
2022-07-19 03:41:11 +0200

    Core: Wait: use struct over tuple for WaitResult. Added Fixed time awating. Cleanup api only use repeat action where its needed.

---
+++ Core/GoalsComponent/WaitResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Core
+{
+    public readonly struct WaitResult
+    {
+        public readonly bool timeout;
+        public readonly double elapsedMs;
+
+        public WaitResult(bool timeout, double elapsedMs)
+        {
+            this.timeout = timeout;
+            this.elapsedMs = elapsedMs;
+        }
+
+        public void Deconstruct(out bool timeout, out double elapsedMs)
+        {
+            timeout = this.timeout;
+            elapsedMs = this.elapsedMs;
+        }
+    }
+}
