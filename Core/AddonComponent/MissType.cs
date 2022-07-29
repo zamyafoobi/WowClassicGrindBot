170d1115d51c5ee426e40a447c2ff9cff10aac13
Xian55 <367101+Xian55@users.noreply.github.com>
2022-07-29 10:34:37 +0200

    Core: MissType generate ToStringF()

--- Core/AddonComponent/MissType.cs
+++ Core/AddonComponent/MissType.cs
@@ -2,6 +2,7 @@
 {
     public enum MissType
     {
+        NONE,
         ABSORB,
         BLOCK,
         DEFLECT,
@@ -13,4 +14,23 @@
         REFLECT,
         RESIST
     }
+
+    public static class MissType_Extensions
+    {
+        public static string ToStringF(this MissType value) => value switch
+        {
+            MissType.ABSORB => nameof(MissType.ABSORB),
+            MissType.BLOCK => nameof(MissType.BLOCK),
+            MissType.DEFLECT => nameof(MissType.DEFLECT),
+            MissType.DODGE => nameof(MissType.DODGE),
+            MissType.EVADE => nameof(MissType.EVADE),
+            MissType.IMMUNE => nameof(MissType.IMMUNE),
+            MissType.MISS => nameof(MissType.MISS),
+            MissType.PARRY => nameof(MissType.PARRY),
+            MissType.REFLECT => nameof(MissType.REFLECT),
+            MissType.RESIST => nameof(MissType.RESIST),
+            _ => nameof(MissType.NONE)
+        };
+    }
+
 }
