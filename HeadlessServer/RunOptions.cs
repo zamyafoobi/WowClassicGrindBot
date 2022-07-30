ae8b656e436aa7c5a9383398bfeff126ba66f8be
Xian55 <367101+Xian55@users.noreply.github.com>
2022-07-30 21:13:24 +0200

    HeadlessServer: Added command line parameter to switch between AddonDataProviderType. BlazorServer: updated StartupConfig as well. By default still 'GDI' going to be used due compatibility, however can opt-in to 'DGXI'.

--- HeadlessServer/RunOptions.cs
+++ HeadlessServer/RunOptions.cs
@@ -8,17 +8,14 @@ namespace HeadlessServer
         [Value(0,
             MetaName = "ClassConfig file",
             Required = true,
-            HelpText = "ClassConfiguration file example: Warrior_1.json")]
+            HelpText = "ClassConfiguration file found in 'Json\\class\\'\nexample: Warrior_1.json")]
         public string? ClassConfig { get; set; }
 
         [Option('m',
             "mode",
             Required = false,
             Default = nameof(StartupConfigPathing.Types.Local),
-            HelpText = $@"PPather service: 
-                {nameof(StartupConfigPathing.Types.Local)} | 
-                {nameof(StartupConfigPathing.Types.RemoteV1)} | 
-                {nameof(StartupConfigPathing.Types.RemoteV3)}")]
+            HelpText = $"Navigation services: \n{nameof(StartupConfigPathing.Types.Local)}\n{nameof(StartupConfigPathing.Types.RemoteV1)}\n{nameof(StartupConfigPathing.Types.RemoteV3)}")]
         public string? Mode { get; set; }
 
         [Option('p',
@@ -28,28 +25,35 @@ namespace HeadlessServer
             HelpText = $"World of Warcraft Process Id")]
         public int Pid { get; set; }
 
+        [Option('r',
+            "reader",
+            Required = false,
+            Default = "GDI",
+            HelpText = $"Screen reader backend.\n'{nameof(AddonDataProviderType.GDI)}': is the default, compatible from Win7.\n'{nameof(AddonDataProviderType.DXGI)}': DirectX based works from Win8.")]
+        public AddonDataProviderType? Reader { get; set; }
+
         [Option("hostv1",
             Required = false,
             Default = "localhost",
-            HelpText = $"PPather Remote V1 host")]
+            HelpText = $"Navigation Remote V1 host")]
         public string? Hostv1 { get; set; }
 
         [Option("portv1",
             Required = false,
             Default = 5001,
-            HelpText = $"PPather Remote V1 port")]
+            HelpText = $"Navigation Remote V1 port")]
         public int Portv1 { get; set; }
 
         [Option("hostv3",
             Required = false,
             Default = "127.0.0.1",
-            HelpText = $"PPather Remote V3 host")]
+            HelpText = $"Navigation Remote V3 host")]
         public string? Hostv3 { get; set; }
 
         [Option("portv3",
             Required = false,
             Default = 47111,
-            HelpText = $"PPather Remote V3 port")]
+            HelpText = $"Navigation Remote V3 port")]
         public int Portv3 { get; set; }
     }
 }
