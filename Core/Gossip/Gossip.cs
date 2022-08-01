ade72d40bb290594fd193cba5b8d486b1d6c2b4c
Xian55 <367101+Xian55@users.noreply.github.com>
2022-08-01 18:05:29 +0200

    Core: Gossip: Add ToStringF()

--- Core/Gossip/Gossip.cs
+++ Core/Gossip/Gossip.cs
@@ -14,4 +14,24 @@
         Unlearn,
         Vendor
     }
+
+    public static class Gossip_Extension
+    {
+        public static string ToStringF(this Gossip value) => value switch
+        {
+            Gossip.Banker => nameof(Gossip.Banker),
+            Gossip.Battlemaster => nameof(Gossip.Battlemaster),
+            Gossip.Binder => nameof(Gossip.Binder),
+            Gossip.Gossip => nameof(Gossip.Gossip),
+            Gossip.Healer => nameof(Gossip.Healer),
+            Gossip.Petition => nameof(Gossip.Petition),
+            Gossip.Tabard => nameof(Gossip.Tabard),
+            Gossip.Taxi => nameof(Gossip.Taxi),
+            Gossip.Trainer => nameof(Gossip.Trainer),
+            Gossip.Unlearn => nameof(Gossip.Unlearn),
+            Gossip.Vendor => nameof(Gossip.Vendor),
+            _ => throw new System.NotImplementedException()
+        };
+    }
+
 }
