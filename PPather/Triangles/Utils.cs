69224750bed25b4f990be7d653cfe8248bbfa6da
coolzoom <tommingc@gmail.com>
2022-08-02 15:27:40 +0800

    Replace IntersectsBox function
    https://gist.github.com/StagPoint/76ae48f5d7ca2f9820748d08e55c9806

--- PPather/Triangles/Utils.cs
+++ PPather/Triangles/Utils.cs
@@ -57,17 +57,180 @@ namespace WowTriangles
         }
     }
 
-    internal unsafe class ccode
+    internal unsafe class testIntersectsBox
     {
-        // int triBoxOverlap(float boxcenter[3],float boxhalfsize[3],float triverts[3][3]);
-        [DllImport("MPQ\\ccode.dll")]
-        public static extern int triBoxOverlap(
-            float* boxcenter,
-            float* boxhalfsize,
-            float* trivert0,
-            float* trivert1,
-            float* trivert2
-            );
+        //https://gist.github.com/StagPoint/76ae48f5d7ca2f9820748d08e55c9806
+        public static bool IntersectsBox(Vector3 boxCenter, Vector3 boxExtents, Vector3 a, Vector3 b, Vector3 c)
+        {
+            // From the book "Real-Time Collision Detection" by Christer Ericson, page 169
+            // See also the published Errata at http://realtimecollisiondetection.net/books/rtcd/errata/
+
+            // Translate triangle as conceptually moving AABB to origin
+            var v0 = (a - boxCenter);
+            var v1 = (b - boxCenter);
+            var v2 = (c - boxCenter);
+
+            // Compute edge vectors for triangle
+            var f0 = (v1 - v0);
+            var f1 = (v2 - v1);
+            var f2 = (v0 - v2);
+
+            #region Test axes a00..a22 (category 3)
+
+            // Test axis a00
+            var a00 = new Vector3(0, -f0.Z, f0.Y);
+            var p0 = Vector3.Dot(v0, a00);
+            var p1 = Vector3.Dot(v1, a00);
+            var p2 = Vector3.Dot(v2, a00);
+            var r = boxExtents.Y * Math.Abs(f0.Z) + boxExtents.Z * Math.Abs(f0.Y);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a01
+            var a01 = new Vector3(0, -f1.Z, f1.Y);
+            p0 = Vector3.Dot(v0, a01);
+            p1 = Vector3.Dot(v1, a01);
+            p2 = Vector3.Dot(v2, a01);
+            r = boxExtents.Y * Math.Abs(f1.Z) + boxExtents.Z * Math.Abs(f1.Y);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a02
+            var a02 = new Vector3(0, -f2.Z, f2.Y);
+            p0 = Vector3.Dot(v0, a02);
+            p1 = Vector3.Dot(v1, a02);
+            p2 = Vector3.Dot(v2, a02);
+            r = boxExtents.Y * Math.Abs(f2.Z) + boxExtents.Z * Math.Abs(f2.Y);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a10
+            var a10 = new Vector3(f0.Z, 0, -f0.X);
+            p0 = Vector3.Dot(v0, a10);
+            p1 = Vector3.Dot(v1, a10);
+            p2 = Vector3.Dot(v2, a10);
+            r = boxExtents.X * Math.Abs(f0.Z) + boxExtents.Z * Math.Abs(f0.X);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a11
+            var a11 = new Vector3(f1.Z, 0, -f1.X);
+            p0 = Vector3.Dot(v0, a11);
+            p1 = Vector3.Dot(v1, a11);
+            p2 = Vector3.Dot(v2, a11);
+            r = boxExtents.X * Math.Abs(f1.Z) + boxExtents.Z * Math.Abs(f1.X);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a12
+            var a12 = new Vector3(f2.Z, 0, -f2.X);
+            p0 = Vector3.Dot(v0, a12);
+            p1 = Vector3.Dot(v1, a12);
+            p2 = Vector3.Dot(v2, a12);
+            r = boxExtents.X * Math.Abs(f2.Z) + boxExtents.Z * Math.Abs(f2.X);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a20
+            var a20 = new Vector3(-f0.Y, f0.X, 0);
+            p0 = Vector3.Dot(v0, a20);
+            p1 = Vector3.Dot(v1, a20);
+            p2 = Vector3.Dot(v2, a20);
+            r = boxExtents.X * Math.Abs(f0.Y) + boxExtents.Y * Math.Abs(f0.X);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a21
+            var a21 = new Vector3(-f1.Y, f1.X, 0);
+            p0 = Vector3.Dot(v0, a21);
+            p1 = Vector3.Dot(v1, a21);
+            p2 = Vector3.Dot(v2, a21);
+            r = boxExtents.X * Math.Abs(f1.Y) + boxExtents.Y * Math.Abs(f1.X);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            // Test axis a22
+            var a22 = new Vector3(-f2.Y, f2.X, 0);
+            p0 = Vector3.Dot(v0, a22);
+            p1 = Vector3.Dot(v1, a22);
+            p2 = Vector3.Dot(v2, a22);
+            r = boxExtents.X * Math.Abs(f2.Y) + boxExtents.Y * Math.Abs(f2.X);
+            if (Math.Max(-fmax(p0, p1, p2), fmin(p0, p1, p2)) > r)
+            {
+                return false;
+            }
+
+            #endregion
+
+            #region Test the three axes corresponding to the face normals of AABB b (category 1)
+
+            // Exit if...
+            // ... [-extents.X, extents.X] and [min(v0.X,v1.X,v2.X), max(v0.X,v1.X,v2.X)] do not overlap
+            if (fmax(v0.X, v1.X, v2.X) < -boxExtents.X || fmin(v0.X, v1.X, v2.X) > boxExtents.X)
+            {
+                return false;
+            }
+
+            // ... [-extents.Y, extents.Y] and [min(v0.Y,v1.Y,v2.Y), max(v0.Y,v1.Y,v2.Y)] do not overlap
+            if (fmax(v0.Y, v1.Y, v2.Y) < -boxExtents.Y || fmin(v0.Y, v1.Y, v2.Y) > boxExtents.Y)
+            {
+                return false;
+            }
+
+            // ... [-extents.Z, extents.Z] and [min(v0.Z,v1.Z,v2.Z), max(v0.Z,v1.Z,v2.Z)] do not overlap
+            if (fmax(v0.Z, v1.Z, v2.Z) < -boxExtents.Z || fmin(v0.Z, v1.Z, v2.Z) > boxExtents.Z)
+            {
+                return false;
+            }
+
+            #endregion
+
+            #region Test separating axis corresponding to triangle face normal (category 2)
+
+            var planeNormal = Vector3.Cross(f0, f1);
+            var planeDistance = Vector3.Dot(planeNormal, v0);
+
+            // Compute the projection interval radius of b onto L(t) = b.c + t * p.n
+            r = boxExtents.X * Math.Abs(planeNormal.X)
+                + boxExtents.Y * Math.Abs(planeNormal.Y)
+                + boxExtents.Z * Math.Abs(planeNormal.Z);
+
+            // Intersection occurs when plane distance falls within [-r,+r] interval
+            if (planeDistance > r)
+            {
+                return false;
+            }
+
+            #endregion
+
+            return true;
+        }
+
+        private static float fmin(float a, float b, float c)
+        {
+            return Math.Min(a, Math.Min(b, c));
+        }
+
+        private static float fmax(float a, float b, float c)
+        {
+            return Math.Max(a, Math.Max(b, c));
+        }
     }
 
     public unsafe class Utils
@@ -190,23 +353,18 @@ namespace WowTriangles
         public static bool TestTriangleBoxIntersect(Vector3 vertex0, Vector3 vertex1, Vector3 vertex2,
                                                     Vector3 boxcenter, Vector3 boxhalfsize)
         {
-            int i = 0;
-            float* pcenter = (float*)&boxcenter;
-            float* phalf = (float*)&boxhalfsize;
-            float* ptriangle0 = (float*)&vertex0;
-            float* ptriangle1 = (float*)&vertex1;
-            float* ptriangle2 = (float*)&vertex2;
+            bool i = false;
 
             //int triBoxOverlap(float boxcenter[3],float boxhalfsize[3],float triverts[3][3]);
             try
             {
-                i = ccode.triBoxOverlap(pcenter, phalf, ptriangle0, ptriangle1, ptriangle2);
+                i = testIntersectsBox.IntersectsBox(boxcenter, boxhalfsize, vertex0, vertex1, vertex2);
             }
             catch (Exception e)
             {
                 Console.WriteLine("WTF " + e);
             }
-            if (i == 1) return true;
+            if (i == true) return true;
             return false;
             /*
             Vector min, max;
