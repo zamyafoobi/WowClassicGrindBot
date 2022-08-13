b3087df21cf5ebfb14459d1dd6394b19c0781d03
Xian55 <367101+Xian55@users.noreply.github.com>
2022-08-13 20:43:00 +0200

    CoreTest: Test_NpcNameFinder: separate overlay to its own class

---
+++ CoreTests/NpcNameFinder/NpcNameOverlay.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Core.Goals;
+
+using GameOverlay.Drawing;
+using GameOverlay.Windows;
+
+using SharedLib.Extensions;
+using SharedLib.NpcFinder;
+
+namespace CoreTests
+{
+    public class NpcNameOverlay : IDisposable
+    {
+        private readonly GraphicsWindow window;
+        private readonly Graphics graphics;
+
+        private Font font;
+        private SolidBrush brush;
+        private SolidBrush brushBackground;
+
+        private readonly NpcNameFinder npcNameFinder;
+        private readonly NpcNameTargeting npcNameTargeting;
+
+        private readonly bool debugTargeting;
+        private readonly bool debugSkinning;
+
+        public NpcNameOverlay(IntPtr handle, NpcNameFinder npcNameFinder,
+            NpcNameTargeting npcNameTargeting, bool debugTargeting, bool debugSkinning)
+        {
+            this.npcNameFinder = npcNameFinder;
+            this.npcNameTargeting = npcNameTargeting;
+            this.debugTargeting = debugTargeting;
+            this.debugSkinning = debugSkinning;
+
+            graphics = new Graphics()
+            {
+                MeasureFPS = true,
+                PerPrimitiveAntiAliasing = true,
+                TextAntiAliasing = true,
+                UseMultiThreadedFactories = false,
+                VSync = false,
+                WindowHandle = IntPtr.Zero
+            };
+
+            window = new StickyWindow(handle, graphics)
+            {
+                FPS = 60,
+                AttachToClientArea = true,
+                BypassTopmost = true,
+            };
+            window.SetupGraphics += SetupGraphics;
+            window.DestroyGraphics += DestroyGraphics;
+            window.DrawGraphics += DrawGraphics;
+
+            window.Create();
+        }
+
+        ~NpcNameOverlay()
+        {
+            Dispose(false);
+        }
+
+        #region IDisposable Support
+
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            window.Dispose();
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        private void SetupGraphics(object sender, SetupGraphicsEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            font = graphics.CreateFont("Arial", 16);
+            brushBackground = graphics.CreateSolidBrush(0, 0x27, 0x31, 0);
+
+            if (npcNameFinder.nameType.HasFlag(NpcNames.Enemy))
+                brush = graphics.CreateSolidBrush(
+                    NpcNameFinder.sE_R,NpcNameFinder.sE_G, NpcNameFinder.sE_B);
+            else if (npcNameFinder.nameType.HasFlag(NpcNames.Friendly))
+                brush = graphics.CreateSolidBrush(
+                    NpcNameFinder.sF_R, NpcNameFinder.sF_G, NpcNameFinder.sF_B);
+            else if (npcNameFinder.nameType.HasFlag(NpcNames.Neutral))
+                brush = graphics.CreateSolidBrush(
+                    NpcNameFinder.sN_R, NpcNameFinder.sN_G, NpcNameFinder.sN_B);
+            else if (npcNameFinder.nameType.HasFlag(NpcNames.Corpse))
+                brush = graphics.CreateSolidBrush(
+                    NpcNameFinder.fC_RGB, NpcNameFinder.fC_RGB, NpcNameFinder.fC_RGB);
+        }
+
+        private void DestroyGraphics(object sender, DestroyGraphicsEventArgs e)
+        {
+            brush.Dispose();
+            brushBackground.Dispose();
+            font.Dispose();
+        }
+
+        private void DrawGraphics(object sender, DrawGraphicsEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.ClearScene(brushBackground);
+
+            if (npcNameFinder.NpcCount <= 0)
+                return;
+
+            g.DrawRectangle(brush, npcNameFinder.Area.Left, npcNameFinder.Area.Top, npcNameFinder.Area.Right, npcNameFinder.Area.Bottom, 1);
+
+            int j = 0;
+            foreach (NpcPosition npc in npcNameFinder.Npcs)
+            {
+                g.DrawRectangle(brush, npc.Rect.Left, npc.Rect.Top, npc.Rect.Right, npc.Rect.Bottom, 1);
+                g.DrawText(font, 10, brush, npc.Rect.Left - 20f, npc.Rect.Top, j.ToString());
+                j++;
+
+                if (debugTargeting)
+                {
+                    foreach (var l in npcNameTargeting.locTargeting)
+                    {
+                        g.DrawCircle(brush, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 1);
+                    }
+                }
+
+                if (debugSkinning)
+                {
+                    int c = npcNameTargeting.locFindBy.Length;
+                    int ex = 3;
+                    System.Drawing.Point[] attemptPoints = new System.Drawing.Point[c + (c * ex)];
+                    for (int i = 0; i < c; i += ex)
+                    {
+                        System.Drawing.Point p = npcNameTargeting.locFindBy[i];
+                        attemptPoints[i] = p;
+                        attemptPoints[i + c] = new System.Drawing.Point(npc.Rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
+                        attemptPoints[i + c + 1] = new System.Drawing.Point(-npc.Rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
+                    }
+
+                    foreach (var l in attemptPoints)
+                    {
+                        g.DrawCircle(brush, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 1);
+                    }
+                }
+            }
+        }
+    }
+}
--- CoreTests/NpcNameFinder/Test_NpcNameFinder.cs
+++ CoreTests/NpcNameFinder/Test_NpcNameFinder.cs
@@ -6,14 +6,13 @@ using System.Threading;
 using System.Text;
 using System.Diagnostics;
 using System.Linq;
-using GameOverlay.Windows;
-using System.Collections.Generic;
 using System;
 using SharedLib.Extensions;
 using Core;
 using Game;
 
 #pragma warning disable 0162
+#nullable enable
 
 namespace CoreTests
 {
@@ -42,14 +41,9 @@ namespace CoreTests
         private readonly SolidBrush brush = new(Color.White);
         private readonly Pen whitePen = new(Color.White, 1);
 
-        private readonly GameOverlay.Windows.GraphicsWindow _window;
-        private readonly GameOverlay.Drawing.Graphics _graphics;
+        private readonly NpcNameOverlay? npcNameOverlay;
 
-        private readonly Dictionary<string, GameOverlay.Drawing.SolidBrush> _brushes;
-        private readonly Dictionary<string, GameOverlay.Drawing.Font> _fonts;
-        private readonly Dictionary<string, GameOverlay.Drawing.Image> _images;
-
-        public Test_NpcNameFinder(ILogger logger, NpcNames types)
+        public Test_NpcNameFinder(ILogger logger, NpcNames types, bool useOverlay)
         {
             this.logger = logger;
 
@@ -60,7 +54,7 @@ namespace CoreTests
             npcNameFinder = new(logger, wowScreen, new AutoResetEvent(false));
 
             MockMouseOverReader mouseOverReader = new();
-            npcNameTargeting = new(logger, new(), wowScreen, npcNameFinder, wowProcessInput, mouseOverReader, new NoBlacklist(), null);
+            npcNameTargeting = new(logger, new(), wowScreen, npcNameFinder, wowProcessInput, mouseOverReader, new NoBlacklist(), null!);
 
             npcNameFinder.ChangeNpcType(types);
 
@@ -70,162 +64,21 @@ namespace CoreTests
                 paint = Graphics.FromImage(paintBitmap);
             }
 
-            //game overlay
-            _brushes = new Dictionary<string, GameOverlay.Drawing.SolidBrush>();
-            _fonts = new Dictionary<string, GameOverlay.Drawing.Font>();
-            _images = new Dictionary<string, GameOverlay.Drawing.Image>();
-
-            _graphics = new GameOverlay.Drawing.Graphics()
-            {
-                MeasureFPS = true,
-                PerPrimitiveAntiAliasing = true,
-                TextAntiAliasing = true,
-                UseMultiThreadedFactories = false,
-                VSync = false,
-                WindowHandle = IntPtr.Zero
-            };
-
-            _window = new StickyWindow(wowProcess.Process.MainWindowHandle, _graphics)
-            {
-                FPS = 60,
-                AttachToClientArea = true,
-                BypassTopmost = true,
-            };
-            _window.SetupGraphics += _window_SetupGraphics;
-            _window.DestroyGraphics += _window_DestroyGraphics;
-            _window.DrawGraphics += _window_DrawGraphics;
-
-            _window.Create();
+            if (useOverlay)
+                npcNameOverlay = new(wowProcess.Process.MainWindowHandle, npcNameFinder, npcNameTargeting, debugTargeting, debugSkinning);
         }
 
         ~Test_NpcNameFinder()
         {
-            Dispose(false);
+            Dispose();
         }
-        #region IDisposable Support
-        private bool disposeValue;
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposeValue)
-            { 
-                _window.Dispose();
-                wowScreen.Dispose();
-                wowProcess.Dispose();
 
-                disposeValue = true;
-            }
-           
-        }
         public void Dispose()
         {
-            Dispose(true);
-            GC.SuppressFinalize(this);  
-        }
-        #endregion
+            npcNameOverlay?.Dispose();
 
-
-        private void _window_SetupGraphics(object sender, SetupGraphicsEventArgs e)
-        {
-            var gfx = e.Graphics;
-
-            _brushes["black"] = gfx.CreateSolidBrush(0, 0, 0);
-            _brushes["white"] = gfx.CreateSolidBrush(255, 255, 255);
-            _brushes["background"] = gfx.CreateSolidBrush(0, 0x27, 0x31, 255.0f * 0.0f);
-
-            Console.WriteLine(_window.Handle.ToString("X"));
-
-            // fonts don't need to be recreated since they are owned by the font factory and not the drawing device
-            if (e.RecreateResources) return;
-
-            _fonts.Add("arial", gfx.CreateFont("Arial", 14));
-        }
-
-        private void _window_DestroyGraphics(object sender, DestroyGraphicsEventArgs e)
-        {
-            foreach (var pair in _brushes) pair.Value.Dispose();
-            foreach (var pair in _fonts) pair.Value.Dispose();
-            foreach (var pair in _images) pair.Value.Dispose();
-        }
-
-        private void _window_DrawGraphics(object sender, DrawGraphicsEventArgs e)
-        {
-            var gfx = e.Graphics;
-
-            gfx.ClearScene(_brushes["background"]);
-            GameOverlay.Drawing.SolidBrush brushOL = _brushes["white"];
-            //the Overlay is drawing at correct position
-            gfx.DrawText(_fonts["arial"], 22, brushOL, 0, 0, $"Overlay FPS: {gfx.FPS}");
-            gfx.DrawText(_fonts["arial"], 22, brushOL, 1000, 740, $"LR");
-            if (npcNameFinder.Npcs.Any())
-            {
-
-                //None = 0,
-                //Enemy = 1,
-                //Friendly = 2,
-                //Neutral = 4,
-                //Corpse = 8
-                //we could use white by default, sometimes the same color would confuse the color processing for npcnamefinder
-                if (npcNameFinder.nameType.HasFlag(NpcNames.Neutral))
-                {
-                    brushOL = gfx.CreateSolidBrush(NpcNameFinder.sN_R, NpcNameFinder.sN_G, NpcNameFinder.sN_B);
-                }
-                if (npcNameFinder.nameType.HasFlag(NpcNames.Enemy))
-                {
-                    brushOL = gfx.CreateSolidBrush(NpcNameFinder.sE_R, NpcNameFinder.sE_G, NpcNameFinder.sE_B);
-                }
-                if (npcNameFinder.nameType.HasFlag(NpcNames.Friendly))
-                {
-                    brushOL = gfx.CreateSolidBrush(NpcNameFinder.sF_R, NpcNameFinder.sF_G, NpcNameFinder.sF_B);
-                }
-                if (npcNameFinder.nameType.HasFlag(NpcNames.Corpse))
-                {
-                    brushOL = gfx.CreateSolidBrush(NpcNameFinder.fC_RGB, NpcNameFinder.fC_RGB, NpcNameFinder.fC_RGB);
-                }
-
-                gfx.DrawRectangle(brushOL, npcNameFinder.Area.Left, npcNameFinder.Area.Top, npcNameFinder.Area.Right, npcNameFinder.Area.Bottom, 2.0f);
-
-                //paint.DrawRectangle(whitePen, npcNameFinder.Area);
-
-                int j = 0;
-                foreach (var npc in npcNameFinder.Npcs)
-                {
-                    if (debugTargeting)
-                    {
-
-                        foreach (var l in npcNameTargeting.locTargeting)
-                        {
-                            //paint.DrawEllipse(whitePen, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 5);
-                            gfx.DrawCircle(brushOL, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 2);
-                        }
-                    }
-
-                    if (debugSkinning)
-                    {
-                        int c = npcNameTargeting.locFindBy.Length;
-                        int ex = 3;
-                        Point[] attemptPoints = new Point[c + (c * ex)];
-                        for (int i = 0; i < c; i += ex)
-                        {
-                            Point p = npcNameTargeting.locFindBy[i];
-                            attemptPoints[i] = p;
-                            attemptPoints[i + c] = new Point(npc.Rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
-                            attemptPoints[i + c + 1] = new Point(-npc.Rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
-                        }
-
-                        foreach (var l in attemptPoints)
-                        {
-                            gfx.DrawCircle(brushOL, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 2);
-                        }
-                    }
-
-                    //paint.DrawRectangle(whitePen, npc.Rect);
-                    //paint.DrawString(j.ToString(), font, brushOL, new PointF(npc.Left - 20f, npc.Top));
-
-                    gfx.DrawRectangle(brushOL, npc.Rect.Left, npc.Rect.Top, npc.Rect.Right, npc.Rect.Bottom, 2);
-                    gfx.DrawText(_fonts["arial"], 10, brushOL, npc.Rect.Left - 20f, npc.Rect.Top, j.ToString());
-                    j++;
-                }
-            }
+            wowScreen.Dispose();
+            wowProcess.Dispose();
         }
 
         public void Execute()
