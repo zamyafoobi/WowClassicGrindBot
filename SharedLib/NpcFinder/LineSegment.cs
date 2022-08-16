2fb18aa5d12eb472397ee47ff0bc23da04e97d42
Xian55 <367101+Xian55@users.noreply.github.com>
2022-08-16 18:24:44 +0200

    SharedLib: better suited names

--- SharedLib/NpcFinder/LineSegment.cs
+++ SharedLib/NpcFinder/LineSegment.cs
@@ -1,18 +1,18 @@
 ﻿namespace SharedLib.NpcFinder
 {
-    public readonly struct LineOfNpcName
+    public readonly struct LineSegment
     {
         public readonly int XStart;
         public readonly int Y;
         public readonly int XEnd;
+        public readonly int XCenter;
 
-        public readonly int XCenter => XStart + ((XEnd - XStart) / 2);
-
-        public LineOfNpcName(int xStart, int xend, int y)
+        public LineSegment(int xStart, int xend, int y)
         {
             this.XStart = xStart;
             this.Y = y;
             this.XEnd = xend;
+            XCenter = XStart + ((XEnd - XStart) / 2);
         }
     }
 }
