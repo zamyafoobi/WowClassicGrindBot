e7eb6c25942f7c28da497594671ec5841603390b
Xian55 <367101+Xian55@users.noreply.github.com>
2022-08-24 21:25:28 +0200

    Core: NpcNameTargeting: For Loot and Friendly NPC interact slow down a bit.

--- Core/GoalsComponent/NpcNameTargeting.cs
+++ Core/GoalsComponent/NpcNameTargeting.cs
@@ -13,6 +13,7 @@ namespace Core.Goals
     public class NpcNameTargeting
     {
         private const int FAST_DELAY = 5;
+        private const int INTERACT_DELAY = 25;
 
         private readonly ILogger logger;
         private readonly CancellationToken ct;
@@ -179,12 +180,11 @@ namespace Core.Goals
                 {
                     Point clickPostion = npcNameFinder.ToScreenCoordinates(npc.ClickPoint.X + p.X, npc.ClickPoint.Y + p.Y);
                     input.SetCursorPosition(clickPostion);
-                    ct.WaitHandle.WaitOne(FAST_DELAY);
+                    ct.WaitHandle.WaitOne(INTERACT_DELAY);
 
                     CursorClassifier.Classify(out CursorType cls);
                     if (cursor.Contains(cls))
                     {
-                        ct.WaitHandle.WaitOne(FAST_DELAY);
                         input.InteractMouseOver();
                         logger.LogInformation($"> NPCs found: {npc.Rect}");
                         return true;
