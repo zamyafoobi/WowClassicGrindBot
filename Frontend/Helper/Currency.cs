efa08c8ceec4e5450e0dfebcbb482d31233032d3
Xian55 <367101+Xian55@users.noreply.github.com>
2022-11-18 22:55:39 +0100

    Frontend: Currency: replacement for fixed size array. Updated related classes. BagChanges: No longer waits for DataChanged update but instead anticipates the next expiration time via a Timer.

---
+++ Frontend/Helper/Currency.cs
@@ -0,0 +1,34 @@
+﻿namespace Frontend
+{
+    public readonly struct Currency
+    {
+        public static readonly Currency Empty = new(0);
+
+        public int Gold { get; }
+        public int Silver { get; }
+        public int Copper { get; }
+
+        public Currency(int amount)
+        {
+            int sign = amount < 0 ? -1 : 1;
+
+            Gold = 0;
+            Silver = 0;
+            Copper = Math.Abs(amount);
+
+            if (Copper >= 10000)
+            {
+                Gold = Copper / 10000 * sign;
+                Copper %= 10000;
+            }
+
+            if (Copper >= 100)
+            {
+                Silver = Copper / 100 * sign;
+                Copper %= 100;
+            }
+
+            Copper *= sign;
+        }
+    }
+}
---
+++ Frontend/Helper/ItemPrice.cs
@@ -0,0 +1,13 @@
+﻿namespace Frontend
+{
+    public static class ItemPrice
+    {
+        public static Currency ToSellPrice(int sellPrice)
+        {
+            if (sellPrice == 0)
+                return Currency.Empty;
+
+            return new Currency(sellPrice);
+        }
+    }
+}
