837911cbc582584b3d0c9b60a4f2186682fd872c
Xian55 <367101+Xian55@users.noreply.github.com>
2023-09-22 11:26:56 +0200

    Core: Added ChatReader

---
+++ Core/Chat/ChatReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Core;
+
+public enum ChatMessageType
+{
+    Whisper,
+    Say,
+    Yell,
+    Emote,
+    Party
+}
+
+public readonly record struct ChatMessageEntry(DateTime Time, ChatMessageType Type, string Author, string Message);
+
+public sealed class ChatReader : IReader
+{
+    private const int cMsg = 98;
+    private const int cMeta = 99;
+
+    public ObservableCollection<ChatMessageEntry> Messages { get; } = new();
+
+    // 12 character name
+    // 1 space
+    // 256 maximum message length
+    private readonly StringBuilder sb = new(12 + 1 + 256);
+
+    private int _head;
+
+    public void Update(IAddonDataProvider reader)
+    {
+        int meta = reader.GetInt(cMeta);
+        if (meta == 0)
+        {
+            _head = 0;
+            return;
+        }
+
+        ChatMessageType type = (ChatMessageType)(meta / 1000000);
+        int length = meta % 1000000 / 1000;
+        int head = meta % 1000;
+
+        if (_head != head)
+            _head = head;
+        else if (_head == head)
+            return;
+
+        string part = reader.GetString(cMsg).Replace('@', ' ');
+
+        sb.Append(part);
+
+        if (head + 2 < length)
+            return;
+
+        string text = sb.ToString().ToLowerInvariant();
+        sb.Clear();
+
+        int firstSpaceIdx = text.IndexOf(" ");
+        string author = text.AsSpan(0, firstSpaceIdx).ToString();
+        text = text.AsSpan(firstSpaceIdx).ToString();
+
+        Messages.Add(new(DateTime.Now, type, author, text));
+    }
+}
