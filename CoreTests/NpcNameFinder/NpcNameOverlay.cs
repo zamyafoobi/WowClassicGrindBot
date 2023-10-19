4856286b342f9a22a2c1ab89a0dd205652f8da67
Xian55 <367101+Xian55@users.noreply.github.com>
2023-10-19 18:16:09 +0200

    CoreTests: NpcNameOverlay: improved performance, ability to visualize TargetVsAdd regions.

--- CoreTests/NpcNameFinder/NpcNameOverlay.cs
+++ CoreTests/NpcNameFinder/NpcNameOverlay.cs
@@ -9,49 +9,52 @@ using SharedLib.Extensions;
 using SharedLib.NpcFinder;
 
 using DPoint = System.Drawing.Point;
+using DRectangle = System.Drawing.Rectangle;
 
 namespace CoreTests;
 
-public class NpcNameOverlay : IDisposable
+internal sealed class NpcNameOverlay : IDisposable
 {
     private readonly GraphicsWindow window;
     private readonly Graphics graphics;
 
     private Font font;
-    private SolidBrush brush;
-    private SolidBrush brushBackground;
+    private SolidBrush brushWhite;
+    private SolidBrush brushGrey;
 
     private readonly NpcNameFinder npcNameFinder;
     private readonly NpcNameTargeting npcNameTargeting;
 
     private readonly bool debugTargeting;
     private readonly bool debugSkinning;
+    private readonly bool debugTargetVsAdd;
 
     private const int padding = 6;
     private const float NumberLeftPadding = 20f;
     private const int FontSize = 10;
 
     public NpcNameOverlay(IntPtr handle, NpcNameFinder npcNameFinder,
-        NpcNameTargeting npcNameTargeting, bool debugTargeting, bool debugSkinning)
+        NpcNameTargeting npcNameTargeting, bool debugTargeting, bool debugSkinning, bool debugTargetVsAdd)
     {
         this.npcNameFinder = npcNameFinder;
         this.npcNameTargeting = npcNameTargeting;
         this.debugTargeting = debugTargeting;
         this.debugSkinning = debugSkinning;
+        this.debugTargetVsAdd = debugTargetVsAdd;
 
         graphics = new Graphics()
         {
-            MeasureFPS = true,
-            PerPrimitiveAntiAliasing = true,
-            TextAntiAliasing = true,
+            MeasureFPS = false,
+            PerPrimitiveAntiAliasing = false,
+            TextAntiAliasing = false,
             UseMultiThreadedFactories = false,
-            VSync = false,
+            VSync = true,
             WindowHandle = IntPtr.Zero
         };
 
         window = new StickyWindow(handle, graphics)
         {
-            FPS = 60,
+            FPS = 10,
             AttachToClientArea = true,
             BypassTopmost = true,
         };
@@ -71,7 +74,7 @@ public class NpcNameOverlay : IDisposable
 
     private bool disposed;
 
-    protected virtual void Dispose(bool disposing)
+    public void Dispose(bool disposing)
     {
         if (disposed) return;
 
@@ -92,15 +95,15 @@ public class NpcNameOverlay : IDisposable
         Graphics graphics = e.Graphics;
 
         font = graphics.CreateFont("Arial", 16);
-        brushBackground = graphics.CreateSolidBrush(0, 0x27, 0x31, 0);
 
-        brush = graphics.CreateSolidBrush(255, 255, 255, 255);
+        brushWhite = graphics.CreateSolidBrush(255, 255, 255, 255);
+        brushGrey = graphics.CreateSolidBrush(128, 128, 128, 255);
     }
 
     private void DestroyGraphics(object sender, DestroyGraphicsEventArgs e)
     {
-        brush.Dispose();
-        brushBackground.Dispose();
+        brushWhite.Dispose();
+        brushGrey.Dispose();
         font.Dispose();
     }
 
@@ -108,47 +111,67 @@ public class NpcNameOverlay : IDisposable
     {
         Graphics g = e.Graphics;
 
-        g.ClearScene(brushBackground);
+        g.ClearScene();
 
         if (npcNameFinder.NpcCount <= 0)
             return;
 
-        g.DrawRectangle(brush, npcNameFinder.Area.Left, npcNameFinder.Area.Top, npcNameFinder.Area.Right, npcNameFinder.Area.Bottom, 1);
+        int c = npcNameTargeting.locFindBy.Length;
+        const int ex = 3;
+        Span<DPoint> attemptPoints = stackalloc DPoint[c + (c * ex)];
+
+        DRectangle area = npcNameFinder.Area;
+
+        g.DrawRectangle(brushWhite, area.Left, area.Top, area.Right, area.Bottom, 1);
+
+        if (debugTargetVsAdd)
+        {
+            int sm = npcNameFinder.screenMid;
+            int stb = npcNameFinder.screenTargetBuffer;
+            int sab = npcNameFinder.screenAddBuffer;
+
+            // target area
+            g.DrawLine(brushWhite, new Point(sm - stb, area.Top), new Point(sm - stb, area.Bottom), 1);
+            g.DrawLine(brushWhite, new Point(sm + stb, area.Top), new Point(sm + stb, area.Bottom), 1);
+
+            // adds area
+            g.DrawLine(brushGrey, new Point(sm - sab, area.Top), new Point(sm - sab, area.Bottom), 1);
+            g.DrawLine(brushGrey, new Point(sm + sab, area.Top), new Point(sm + sab, area.Bottom), 1);
+        }
 
         for (int i = 0; i < npcNameFinder.Npcs.Count; i++)
         {
             NpcPosition npc = npcNameFinder.Npcs[i];
+            DRectangle rect = npc.Rect;
 
-            g.DrawRectangle(brush, npc.Rect.Left - padding, npc.Rect.Top - padding, npc.Rect.Right + padding, npc.Rect.Bottom + padding, 1);
-            g.DrawText(font, FontSize, brush, npc.Rect.Left - NumberLeftPadding - padding, npc.Rect.Top - padding, i.ToString());
+            g.DrawRectangle(!debugTargetVsAdd ? brushWhite : npcNameFinder.IsAdd(npc) ? brushGrey : brushWhite,
+                rect.Left - padding, rect.Top - padding, rect.Right + padding, rect.Bottom + padding, 1);
+
+            g.DrawText(font, FontSize, brushWhite, rect.Left - NumberLeftPadding - padding, rect.Top - padding, i.ToString());
 
             if (debugTargeting)
             {
                 for (int k = 0; k < npcNameTargeting.locTargeting.Length; k++)
                 {
-                    DPoint l = npcNameTargeting.locTargeting[k];
-                    g.DrawCircle(brush, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 1);
+                    DPoint p = npcNameTargeting.locTargeting[k];
+                    g.DrawCircle(brushWhite, p.X + npc.ClickPoint.X, p.Y + npc.ClickPoint.Y, 5, 1);
                 }
             }
 
             if (debugSkinning)
             {
-                int c = npcNameTargeting.locFindBy.Length;
-                const int ex = 3;
-
-                DPoint[] attemptPoints = new DPoint[c + (c * ex)];
                 for (int j = 0; j < c; j += ex)
                 {
                     DPoint p = npcNameTargeting.locFindBy[j];
                     attemptPoints[j] = p;
-                    attemptPoints[j + c] = new DPoint(npc.Rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
-                    attemptPoints[j + c + 1] = new DPoint(-npc.Rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
+                    attemptPoints[j + c] = new DPoint(rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
+                    attemptPoints[j + c + 1] = new DPoint(-rect.Width / 2, p.Y).Scale(npcNameFinder.ScaleToRefWidth, npcNameFinder.ScaleToRefHeight);
                 }
 
                 for (int k = 0; k < attemptPoints.Length; k++)
                 {
-                    DPoint l = attemptPoints[k];
-                    g.DrawCircle(brush, l.X + npc.ClickPoint.X, l.Y + npc.ClickPoint.Y, 5, 1);
+                    DPoint p = attemptPoints[k];
+                    g.DrawCircle(brushWhite, p.X + npc.ClickPoint.X, p.Y + npc.ClickPoint.Y, 5, 1);
                 }
             }
         }
