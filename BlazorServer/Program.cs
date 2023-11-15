5cb442ce4251d7ed9dcaa44dbfe1fb0cc7c9b361
Xian55 <367101+Xian55@users.noreply.github.com>
2023-11-15 14:27:37 +0100

    Refactor: BlazorServer upgrade to net8.0
    BlazorServer: Remove Startup
    
    BlazorServer: use InteractiveServerRenderMode with Routes

--- BlazorServer/Program.cs
+++ BlazorServer/Program.cs
@@ -1,18 +1,28 @@
 using System;
+using System.Threading;
 
+using Core;
+
+using Frontend;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using Serilog;
+using Serilog.Events;
+using Serilog.Templates.Themes;
+using Serilog.Templates;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace BlazorServer;
 
 public static class Program
 {
-    private const string hostUrl = "http://0.0.0.0:5000";
-
     public static void Main(string[] args)
     {
         while (true)
@@ -20,7 +30,7 @@ public static class Program
             Log.Information($"[{nameof(Program),-15}] Starting blazor server");
             try
             {
-                IHost host = CreateHostBuilder(args).Build();
+                IHost host = CreateApp(args);
                 var logger = host.Services.GetRequiredService<Microsoft.Extensions.Logging.ILogger>();
 
                 AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs args) =>
@@ -35,18 +45,110 @@ public static class Program
             {
                 Log.Information($"[{nameof(Program),-15}] {ex.Message}");
                 Log.Information("");
-                System.Threading.Thread.Sleep(3000);
+
+                Thread.Sleep(3000);
             }
         }
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseUrls(hostUrl);
-                webBuilder.ConfigureLogging(logging =>
-                    logging.ClearProviders().AddSerilog());
-                webBuilder.UseStartup<Startup>();
-            });
+    private static WebApplication CreateApp(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders().AddSerilog();
+
+        ConfigureServices(builder.Configuration, builder.Services);
+
+        return ConfigureApp(builder, builder.Environment);
+    }
+
+    private static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+    {
+        ILoggerFactory logFactory = LoggerFactory.Create(builder =>
+        {
+            builder.ClearProviders().AddSerilog();
+        });
+
+        services.AddLogging(builder =>
+        {
+            LoggerSink sink = new();
+            builder.Services.AddSingleton(sink);
+
+            const string outputTemplate = "[{@t:HH:mm:ss:fff} {@l:u1}] {#if Length(SourceContext) > 0}[{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1),-15}] {#end}{@m}\n{@x}";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Sink(sink)
+                .WriteTo.File(new ExpressionTemplate(outputTemplate),
+                    "out.log",
+                    rollingInterval: RollingInterval.Day)
+                .WriteTo.Debug(new ExpressionTemplate(outputTemplate))
+                .WriteTo.Console(new ExpressionTemplate(outputTemplate, theme: TemplateTheme.Literate))
+                .CreateLogger();
+
+            builder.Services.AddSingleton<Microsoft.Extensions.Logging.ILogger>(logFactory.CreateLogger(string.Empty));
+        });
+
+        Microsoft.Extensions.Logging.ILogger log = logFactory.CreateLogger("Program");
+
+        log.LogInformation(
+            $"{Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName} " +
+            $"{DateTimeOffset.Now}");
+
+        services.AddStartupConfigurations(configuration);
+
+        services.AddWoWProcess(log);
+
+        services.AddCoreBase();
+
+        if (AddonConfig.Exists() && FrameConfig.Exists())
+        {
+            services.AddCoreNormal(log);
+        }
+        else
+        {
+            services.AddCoreConfiguration(log);
+        }
+
+        services.AddFrontend();
+
+        services.AddCoreFrontend();
+
+        services.BuildServiceProvider(
+            new ServiceProviderOptions { ValidateOnBuild = true });
+    }
+
+    private static WebApplication ConfigureApp(WebApplicationBuilder builder, IWebHostEnvironment env)
+    {
+        WebApplication app = builder.Build();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseStaticFiles();
+
+        DataConfig dataConfig = app.Services.GetRequiredService<DataConfig>();
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, dataConfig.Path)),
+            RequestPath = "/path"
+        });
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddAdditionalAssemblies(typeof(Frontend._Imports).Assembly);
+
+        app.UseAntiforgery();
+
+        return app;
+    }
+
 }
